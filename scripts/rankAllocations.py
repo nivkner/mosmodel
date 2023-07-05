1acab67b0e01661455db92bb4191c6f42b8c5ec0
Niv Kaminer <niv.kaminer@campus.technion.ac.il>
2023-07-05 21:21:58 +0300

    use arange to join instead of cross join + filter

--- scripts/rankAllocations.py
+++ scripts/rankAllocations.py
@@ -73,7 +73,7 @@ def rank_allocations(allocs: Path, pebs: Path, base: Path) -> pl.DataFrame:
 
     # match a pebs entry with the allocation overlapping with its page
     # by first creating a cross product of the two, filtering by whether the page is containing in the pages containing the allocation
-    allocs_with_misses_df = allocs_pages_df.join(pebs_normalized_df, on=1).filter((pl.col("start") <= pl.col("PAGE_NUMBER")) & (pl.col("end") >= pl.col("PAGE_NUMBER")))
+    allocs_with_misses_df = allocs_pages_df.with_columns(PAGE_NUMBER=pl.arange(pl.col("start"), pl.col("end"))).join(pebs_normalized_df, on="PAGE_NUMBER")
     # find the allocation contexts that cause the most tlb misses for the least number of memory usage (so that we can choose the handlful that are most effective)
     return allocs_with_misses_df.collect() \
                                 .groupby("context") \
