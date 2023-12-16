ec387ae28ee4b23dd58239513bb32f13ae6ab195
Niv Kaminer <niv.kaminer@campus.technion.ac.il>
2023-12-17 00:46:09 +0200

    override huge page reservation in benchmark generation

--- scripts/generate_benchmark.py
+++ scripts/generate_benchmark.py
@@ -15,6 +15,7 @@ parser.add_argument('-a', '--allocator', type=Path, help="the path to the custom
 parser.add_argument('-d', '--description', type=str, help="a string describing the benchmark, for posterity")
 parser.add_argument('-t', '--thp', action='store_true', help="flag to run the command with THP enabled")
 parser.add_argument('-e', '--env_vars', type=str, nargs='+', default=[], metavar="VAR=VALUE", help="a list of env vars to be passed to the allocator")
+parser.add_argument('-r', '--reserve', type=int, help="override the number of huge pages reserved by mosmodel")
 args = parser.parse_args()
 
 custom_allocator = f"./{args.allocator.name}" if args.allocator is not None else ""
@@ -40,3 +41,7 @@ if args.allocator is not None:
 if args.description is not None:
     assert not args.output.joinpath("description.txt").exists()
     args.output.joinpath("description.txt").write_text(args.description)
+
+if args.reserve is not None:
+    assert not args.output.joinpath("reserve.cfg").exists()
+    args.output.joinpath("reserve.cfg").write_text(f"{args.reserve},0")
