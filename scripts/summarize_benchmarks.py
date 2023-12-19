820c9857608d28c4362761f376d75e861a740a2d
Niv Kaminer <niv.kaminer@campus.technion.ac.il>
2023-12-19 22:59:21 +0200

    add meminfo to summary

--- scripts/summarize_benchmarks.py
+++ scripts/summarize_benchmarks.py
@@ -20,7 +20,8 @@ benchmark_dfs = []
 for benchmark in args.input.iterdir():
     benchmark_time_df = pl.scan_csv(benchmark.joinpath("repeat*/time.out"), has_header=False, new_columns=["Measure", "Value"])
     benchmark_perf_df = pl.scan_csv(benchmark.joinpath("repeat*/perf.out"), has_header=False, skip_rows=2, new_columns=["Value", "Unit", "Measure"]).select("Measure", pl.col("Value").cast(pl.Float64))
-    benchmark_df = pl.concat([benchmark_time_df, benchmark_perf_df])
+    benchmark_meminfo_df = pl.scan_csv(benchmark.joinpath("repeat*/meminfo.out"), has_header=False, new_columns=["Measure", "Value"], dtypes=[pl.Utf8, pl.Float64])
+    benchmark_df = pl.concat([benchmark_time_df, benchmark_perf_df, benchmark_meminfo_df])
 
     benchmark_df = benchmark_df.group_by("Measure").agg(pl.col("Value").mean().alias("Mean"), pl.col("Value").std().alias("StdDev"), pl.lit(benchmark.name).alias("Benchmark"))
 
