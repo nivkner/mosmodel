76a0bc7485f446c474f8eb56d52916cdb155e867
Niv Kaminer <niv.kaminer@campus.technion.ac.il>
2024-01-04 04:00:46 +0200

    summarize when running script

--- scripts/run_benchmark_collection.py
+++ scripts/run_benchmark_collection.py
@@ -13,6 +13,7 @@ parser = argparse.ArgumentParser(description='run a collection of benchmarks and
 
 parser.add_argument('benchmarks', metavar='BENCHMARK', type=Path, nargs='+', help='a benchmark in the collection')
 parser.add_argument('-o', '--output', type=Path, required=True, help="the path where the output of the benchmarks will be stored")
+parser.add_argument('-s', '--summary', type=Path, help="the path where the summary of the benchmarks will be stored")
 args = parser.parse_args()
 
 run_timestamp = datetime.datetime.now()
@@ -23,3 +24,8 @@ for benchmark in args.benchmarks:
     subprocess.run(["make", f"BENCHMARK_PATH={benchmark}", "experiments/single_page_size/layout2mb"], check=True)
     shutil.copytree("experiments/single_page_size/layout2mb", run_dir.joinpath(benchmark.name))
     subprocess.run(["make", "clean"], check=True)
+
+if args.summary is not None:
+    subprocess.run([scripts_dir.joinpath("summarize_benchmarks.py"), "-i", run_dir, "-o", args.summary], check=True)
+    summary_file = args.summary.joinpath(run_dir.name + "_summary.csv")
+    print(f"created summary at {summary_file}")
