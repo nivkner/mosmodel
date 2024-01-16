3afdd52e12faa895e3f377643f5817489bee97d9
Niv Kaminer <niv.kaminer@campus.technion.ac.il>
2024-01-16 19:40:48 +0200

    add data from json from redis if available

--- scripts/summarize_benchmarks.py
+++ scripts/summarize_benchmarks.py
@@ -8,11 +8,24 @@ import os
 import subprocess
 import polars as pl
 
+def get_memtier_df_from_json(memtier_json_path):
+    with open(memtier_json_path) as json_file:
+        js = json.load(json_file)
+        bench_stats = js["ALL STATS"]["Totals"]
+        bench_stats.pop("Time-Serie")
+        percentiles = bench_stats.pop("Percentile Latencies")
+        percentiles.pop("Histogram log format")
+        bench_stats = {**bench_stats, **({"Latency "+key: val for (key,val) in percentiles.items()})}
+        return pl.LazyFrame(bench_stats).melt(variable_name="Measure", value_name="Value")
+
 def summarize_benchmark(benchmark_dir):
     df_types = []
     df_types.append(pl.scan_csv(benchmark_dir.joinpath("repeat*/time.out"), has_header=False, new_columns=["Measure", "Value"]))
     df_types.append(pl.scan_csv(benchmark_dir.joinpath("repeat*/perf.out"), has_header=False, skip_rows=2, new_columns=["Value", "Unit", "Measure"]).select("Measure", pl.col("Value").cast(pl.Float64)))
     df_types.append(pl.scan_csv(benchmark_dir.joinpath("repeat*/meminfo.out"), has_header=False, new_columns=["Measure", "Value"], dtypes=[pl.Utf8, pl.Float64]))
+
+    for memtier_json in benchmark_dir.glob("repeat*/bench.json"):
+        df_types.append(get_memtier_df_from_json(memtier_json))
 
     benchmark_df = pl.concat(df_types)
 
