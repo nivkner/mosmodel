d807468b804ea2a6b19e8af7ce5dc7a8efd792f1
Niv Kaminer <niv.kaminer@campus.technion.ac.il>
2024-01-16 19:35:33 +0200

    move summerizing a single benchmark to its own function

--- scripts/summarize_benchmarks.py
+++ scripts/summarize_benchmarks.py
@@ -1,26 +1,25 @@
 #!/usr/bin/env python3
 
 import argparse
+import json
 from pathlib import Path
 import shutil
 import os
 import subprocess
 import polars as pl
 
+def summarize_benchmark(benchmark_dir):
+    df_types = []
+    df_types.append(pl.scan_csv(benchmark_dir.joinpath("repeat*/time.out"), has_header=False, new_columns=["Measure", "Value"]))
+    df_types.append(pl.scan_csv(benchmark_dir.joinpath("repeat*/perf.out"), has_header=False, skip_rows=2, new_columns=["Value", "Unit", "Measure"]).select("Measure", pl.col("Value").cast(pl.Float64)))
+    df_types.append(pl.scan_csv(benchmark_dir.joinpath("repeat*/meminfo.out"), has_header=False, new_columns=["Measure", "Value"], dtypes=[pl.Utf8, pl.Float64]))
+
+    benchmark_df = pl.concat(df_types)
+
+    return benchmark_df.group_by("Measure").agg(pl.col("Value").mean().alias("Mean"), pl.col("Value").std().alias("StdDev"), pl.lit(benchmark_dir.name).alias("Benchmark"))
+
 def summarize_benchmarks(outputs_dir):
-    for benchmark in outputs_dir.iterdir():
-        df_types = []
-        df_types.append(pl.scan_csv(benchmark.joinpath("repeat*/time.out"), has_header=False, new_columns=["Measure", "Value"]))
-        df_types.append(pl.scan_csv(benchmark.joinpath("repeat*/perf.out"), has_header=False, skip_rows=2, new_columns=["Value", "Unit", "Measure"]).select("Measure", pl.col("Value").cast(pl.Float64)))
-        df_types.append(pl.scan_csv(benchmark.joinpath("repeat*/meminfo.out"), has_header=False, new_columns=["Measure", "Value"], dtypes=[pl.Utf8, pl.Float64]))
-
-        benchmark_df = pl.concat(df_types)
-
-        benchmark_df = benchmark_df.group_by("Measure").agg(pl.col("Value").mean().alias("Mean"), pl.col("Value").std().alias("StdDev"), pl.lit(benchmark.name).alias("Benchmark"))
-
-        benchmark_dfs.append(benchmark_df)
-
-    return pl.concat(benchmark_dfs)
+    return pl.concat([summarize_benchmark(benchmark_dir) for benchmark_dir in outputs_dir.iterdir()])
 
 
 if __name__ == "__main__":
