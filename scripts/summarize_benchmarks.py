045ab9593ee4159b63cb9418cae9cc75b2d692f3
Niv Kaminer <niv.kaminer@campus.technion.ac.il>
2024-01-16 18:40:09 +0200

    refactor benchmark summary

--- scripts/summarize_benchmarks.py
+++ scripts/summarize_benchmarks.py
@@ -7,28 +7,31 @@ import os
 import subprocess
 import polars as pl
 
-scripts_dir = Path(__file__).parent
+def summarize_benchmarks(outputs_dir):
+    for benchmark in outputs_dir.iterdir():
+        df_types = []
+        df_types.append(pl.scan_csv(benchmark.joinpath("repeat*/time.out"), has_header=False, new_columns=["Measure", "Value"]))
+        df_types.append(pl.scan_csv(benchmark.joinpath("repeat*/perf.out"), has_header=False, skip_rows=2, new_columns=["Value", "Unit", "Measure"]).select("Measure", pl.col("Value").cast(pl.Float64)))
+        df_types.append(pl.scan_csv(benchmark.joinpath("repeat*/meminfo.out"), has_header=False, new_columns=["Measure", "Value"], dtypes=[pl.Utf8, pl.Float64]))
 
-parser = argparse.ArgumentParser(description='summarize all of the information gathered by mosmodel on a given run')
+        benchmark_df = pl.concat(df_types)
 
-parser.add_argument('-i', '--input', type=Path, required=True, help="the path where the results of a run are stored")
-parser.add_argument('-o', '--output', type=Path, required=True, help="the path where the summary will be stored")
-args = parser.parse_args()
+        benchmark_df = benchmark_df.group_by("Measure").agg(pl.col("Value").mean().alias("Mean"), pl.col("Value").std().alias("StdDev"), pl.lit(benchmark.name).alias("Benchmark"))
 
-benchmark_dfs = []
+        benchmark_dfs.append(benchmark_df)
 
-for benchmark in args.input.iterdir():
-    benchmark_time_df = pl.scan_csv(benchmark.joinpath("repeat*/time.out"), has_header=False, new_columns=["Measure", "Value"])
-    benchmark_perf_df = pl.scan_csv(benchmark.joinpath("repeat*/perf.out"), has_header=False, skip_rows=2, new_columns=["Value", "Unit", "Measure"]).select("Measure", pl.col("Value").cast(pl.Float64))
-    benchmark_meminfo_df = pl.scan_csv(benchmark.joinpath("repeat*/meminfo.out"), has_header=False, new_columns=["Measure", "Value"], dtypes=[pl.Utf8, pl.Float64])
-    benchmark_df = pl.concat([benchmark_time_df, benchmark_perf_df, benchmark_meminfo_df])
+    return pl.concat(benchmark_dfs)
 
-    benchmark_df = benchmark_df.group_by("Measure").agg(pl.col("Value").mean().alias("Mean"), pl.col("Value").std().alias("StdDev"), pl.lit(benchmark.name).alias("Benchmark"))
 
-    benchmark_dfs.append(benchmark_df)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='summarize all of the information gathered by mosmodel on a given run')
 
-summary_df = pl.concat(benchmark_dfs)
+    parser.add_argument('-i', '--input', type=Path, required=True, help="the path where the results of a run are stored")
+    parser.add_argument('-o', '--output', type=Path, required=True, help="the path where the summary will be stored")
+    args = parser.parse_args()
 
-args.output.mkdir(parents=True, exist_ok=True)
+    summary_df = summarize_benchmarks(args.input)
 
-summary_df.collect(streaming=True).write_csv(args.output.joinpath(args.input.name + "_summary.csv"))
+    args.output.mkdir(parents=True, exist_ok=True)
+
+    summary_df.collect(streaming=True).write_csv(args.output.joinpath(args.input.name + "_summary.csv"))
