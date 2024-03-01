2e8fd5501694200266d668e6a5a33b5f5340e2eb
Niv Kaminer <niv.kaminer@campus.technion.ac.il>
2024-03-01 11:53:21 +0200

    add support for ingens in wrapper

--- scripts/run_wrapper.sh
+++ scripts/run_wrapper.sh
@@ -8,11 +8,29 @@ if [[ "$PWD" != *"/experiments/memory_footprint/"* ]]; then
 fi
 
 if [[ "${USE_THP:-0}" == "1" ]]; then
-	echo "always" | sudo tee /sys/kernel/mm/transparent_hugepage/enabled
+	sudo bash -c "echo always > /sys/kernel/mm/transparent_hugepage/enabled
+	echo always > /sys/kernel/mm/transparent_hugepage/defrag
+	echo 1 > /sys/kernel/mm/transparent_hugepage/khugepaged/defrag
+	if [[ -e /sys/kernel/mm/transparent_hugepage/ingens ]]; then
+		echo 0 > /sys/kernel/mm/transparent_hugepage/ingens/deferred_mode
+		echo 0 > /sys/kernel/mm/transparent_hugepage/ingens/util_threshold
+		echo 0 > /sys/kernel/mm/transparent_hugepage/ingens/compact_sleep_millisecs
+	fi"
+fi
+
+if [[ "${USE_INGENS:-0}" == "1" ]]; then
+	sudo bash -c "echo always > /sys/kernel/mm/transparent_hugepage/enabled
+	echo always > /sys/kernel/mm/transparent_hugepage/defrag
+	echo 1 > /sys/kernel/mm/transparent_hugepage/khugepaged/defrag
+	if [[ -e /sys/kernel/mm/transparent_hugepage/ingens ]]; then
+		echo 1 > /sys/kernel/mm/transparent_hugepage/ingens/deferred_mode
+		echo 90 > /sys/kernel/mm/transparent_hugepage/ingens/util_threshold
+		echo 0 > /sys/kernel/mm/transparent_hugepage/ingens/compact_sleep_millisecs
+	fi"
 fi
 
 source real_run.sh
 
-if [[ "${USE_THP:-0}" == "1" ]]; then
+if [[ "${USE_THP:-0}" == "1" || "${USE_INGENS:-0}" == "1" ]]; then
 	echo "never" | sudo tee /sys/kernel/mm/transparent_hugepage/enabled
 fi
