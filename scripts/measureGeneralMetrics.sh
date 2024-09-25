5af1ce1c80e6ba717af7baaca781dbd4668b7348
Niv Kaminer <niv.kaminer@campus.technion.ac.il>
2024-09-26 01:36:49 +0300

    measue less events in perf

--- scripts/measureGeneralMetrics.sh
+++ scripts/measureGeneralMetrics.sh
@@ -15,7 +15,7 @@ function cleanup {
         fi
 }
 
-general_events="ref-cycles,cpu-cycles:u,cpu-cycles:k,instructions,"
+general_events="ref-cycles,instructions,page-faults,cache-misses,cache-references,"
 
 # We no longer measure the cache events because we want to reduce sampling and improve the measuring accuracy.
 #general_events+=",L1-dcache-loads,L1-dcache-stores,L1-dcache-load-misses,L1-dcache-store-misses"
@@ -26,8 +26,6 @@ prefix_perf_command="perf stat --field-separator=, --output=perf.out"
 #dtlb_events=`perf list | \grep -o "dtlb_.*_misses\.\w*" | sort -u | tr '\n' ','`
 #dtlb_events=${dtlb_events%?} # remove the trailing , charachter
 #dtlb_events=dtlb_load_misses.miss_causes_a_walk,dtlb_load_misses.walk_duration,dtlb_store_misses.miss_causes_a_walk,dtlb_store_misses.walk_duration
-
-dtlb_events=dtlb_load_misses.miss_causes_a_walk:kP,dtlb_load_misses.walk_active:kP,dtlb_store_misses.miss_causes_a_walk:kP,dtlb_store_misses.walk_active:kP,dtlb_load_misses.miss_causes_a_walk:uP,dtlb_load_misses.walk_active:uP,dtlb_store_misses.miss_causes_a_walk:uP,dtlb_store_misses.walk_active:uP
 
 dtlb_events=dtlb_load_misses.miss_causes_a_walk:P,dtlb_store_misses.miss_causes_a_walk:P
 
